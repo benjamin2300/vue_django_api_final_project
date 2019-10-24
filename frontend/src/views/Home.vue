f38871a8143e4c4d5b108818a8de05748bec695a
finch <finch2300@yahoo.com>
2019-10-24 08:38:32 +0800

    L74 Load More Question Button

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -15,6 +15,16 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
+      <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestions"
+          class="btn btn-sm btn-outline-success"
+          >Load More
+
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -25,15 +35,27 @@ export default {
   name: "home",
   data() {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     getQuestions() {
       let endpoint = "/api/questions/";
+      if (this.next) {
+        endpoint = this.next; 
+      }
+      this.loadingQuestions = true;
       apiService(endpoint)
       .then(data => {
         this.questions.push(...data.results);
+        this.loadingQuestions = false;
+        if (data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
       })
     }
   },
